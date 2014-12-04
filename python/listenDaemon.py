6f60325543a1d6c54f52ce9a302dae88900b7911
Patrick Conner <pconner03@gmail.com>
2014-12-03 22:55:37 -0500

    Parses knock from arduino

--- python/listenDaemon.py
+++ python/listenDaemon.py
@@ -12,10 +12,32 @@ from email.utils import parsedate
 import hashlib
 import serial
 
+twilioNumber = ""
+client = None
+
+LISTEN_COMMAND = "1"
+UNLOCK_COMMAND = "2"
+
+SHORT_RANGE = (0,33)
+MEDIUM_RANGE = (34, 66)
+LONG_RANGE = (67, 100)
+
+SHORT = "0"
+MEDIUM = "1"
+LONG = "2"
+'''
+0 is short
+1 is medium
+2 is long
+'''
+
+TEST_KNOCK = "100121"
 
 
 def main():
-	TIME_TO_SLEEP = 25.0 #poll every 25 seconds
+	global twilioNumber
+	global client
+	TIME_TO_SLEEP = 5.0 #poll every 5 seconds
 	client = TRC(credentials.account_sid, credentials.auth_token)
 
 	twilioNumber = client.phone_numbers.list()[0].phone_number
@@ -41,6 +63,9 @@ def main():
 		time.sleep(TIME_TO_SLEEP)
 
 def parseText(num, password):
+	global twilioNumber
+	global UNLOCK_COMMAND
+	global LISTEN_COMMAND
 	con = sqlite3.connect("doorlock.db")
 	cur = con.cursor()
 	query = "SELECT passwordHash FROM users WHERE number='" + num+ "';"
@@ -51,16 +76,50 @@ def parseText(num, password):
 	else:
 		if hashlib.sha256(password).hexdigest() == result[0]:
 			print "Valid attempt!!!"
-			ser = serial.Serial('/dev/tty.usbmodemfa131', 9600)
+			ser = serial.Serial('/dev/ttyACM0', 9600)
 			angle = 0
-			while 1:
-				# angle += 45
-				# if(angle == 360):
-				# 	angle = 0
-				# ser.write(str(angle))
-				ser.write(str(1))
-				print ser.readline()
+			confirmMessage = client.messages.create(to=num, from_=twilioNumber,body="Valid password. Start knocking!" )
+			ser.write(LISTEN_COMMAND)
+			arrayString = ser.readline()
+			print "Got string " + arrayString
+			distanceArray = [int(i) for i in arrayString.split(",")[:-1] if i != "0"]
+			if len(distanceArray) == 0:
+				print "Knock timed out"
+				return
+			distanceArray = convertToRatio(distanceArray)
+			print "Distance percentages"
+			print distanceArray			
+
+			secretKnock = TEST_KNOCK #TODO - get from database
+			if len(distanceArray) != len(secretKnock):
+				print "Incorrect knock (length)"
+				return
+			for i in range(len(distanceArray)):
+				if not inRange(secretKnock[i], distanceArray[i]):
+					print "Incorrect knock (sequence)"
+			print "Correct knock!"
+			ser.write(UNLOCK_COMMAND)
+
 		else:
 			print "Invalid attempt by known user"
+
+def convertToRatio(distanceArray):
+	mx = distanceArray[0]
+	for i in distanceArray:
+		if i > mx:
+			mx = i
+	for i in range(len(distanceArray)):
+		distanceArray[i] = int((float(distanceArray[i])/mx) * 100)
+	return distanceArray
+
+
+def inRange(type, percent):
+	if type == SHORT:
+		tup = SHORT_RANGE
+	elif type == MEDIUM:
+		tup = MEDIUM_RANGE
+	else:
+		tup = LONG_RANGE
+	return percent >= tup[0] and percent <= tup[1]
 
 main()
